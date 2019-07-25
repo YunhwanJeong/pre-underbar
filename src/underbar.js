5cfd9fb4f9377faf4b46a5df56a9eafe9220854c
Yun Hwan Chung <yun_hwan_chung@bat.com>
2019-07-25 17:55:30 +0900

    SYNC

--- src/underbar.js
+++ src/underbar.js
@@ -52,6 +52,9 @@
   // Note 2: 이 문제를 풀기 위해서는 여러분이 spec 디렉토리에 있는 테스트 케이스의 요구사항을 잘 살펴볼 필요가 있습니다.
   // 실제로 어떻게 사용되는지 각 테스트 케이스 항목에 잘 나와 있습니다.
   _.each = function(collection, iterator) {
+    for (let key in collection) {
+      iterator.call(key);
+    }
   };
 
   // target으로 전달되는 값이 array에서 발견되면, 그 index를 리턴하세요.
